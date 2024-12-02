7d9b00613c838e74cd49f29c457542174e774a81
Mohamed Alaga <malaga.142014@gmail.com>
2024-12-02 23:17:04 +0200

    0. Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,12 +4,23 @@ from typing import Any, Callable, Union
 
 
 class Cache:
+    """
+    class to handle caching to redis db
+    """
     _r = redis.Redis()
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """
+        method to initialize redis db
+        """
         self._r.flushdb(True)
 
-    def store(self, data : Union[str,bytes,int,float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        method to store data to redis db
+        :param data: data to store
+        :return: the id of the stored data
+        """
         key = str(uuid.uuid4())
         self._r.set(key, data)
         return key
